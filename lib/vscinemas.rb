df73258abfea6560d7e9f4c975b02184fdcdbf7b
蒼時弦也 <elct9620@frost.tw>
2021-12-20 22:01:15 +0800

    Initialize Project

---
+++ lib/vscinemas.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'vscinemas/version'
+
+module VSCinemas
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/vscinemas_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe VSCinemas do
+  it 'has a version number' do
+    expect(VSCinemas::VERSION).not_to be nil
+  end
+end
